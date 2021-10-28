31986ccb229e7a72de3b7a944a671733bc231a1b
Warlord <201951087@iiitvadodara.ac.in>
2021-10-28 12:48:10 +0530

    er

--- movieapp/views.py
+++ movieapp/views.py
@@ -5,45 +5,53 @@ from django.http import HttpResponse
 import requests
 
 
-
 def index(request):
 
-    soon_url="https://imdb-api.com/en/API/ComingSoon/k_pftzqnp0"
-    soon=requests.get(soon_url).json()
-    release_url=("https://imdb-api.com/en/API/InTheaters/k_pftzqnp0")
-    release=requests.get(release_url).json()
-    top_url="https://imdb-api.com/en/API/BoxOffice/k_pftzqnp0"
-    top=requests.get(top_url).json()
-    lastest_url="https://imdb-api.com/en/API/InTheaters/k_pftzqnp0"
-    lastest=requests.get(lastest_url).json()
-    context={"soon":soon, "release": release, "top": top, "lastest": lastest}
-    return render(request,"home.html",context)
+    soon_url = "https://imdb-api.com/en/API/ComingSoon/k_pftzqnp0"
+    soon = requests.get(soon_url).json()
+    release_url = ("https://imdb-api.com/en/API/InTheaters/k_pftzqnp0")
+    release = requests.get(release_url).json()
+    top_url = "https://imdb-api.com/en/API/BoxOffice/k_pftzqnp0"
+    top = requests.get(top_url).json()
+    lastest_url = "https://imdb-api.com/en/API/InTheaters/k_pftzqnp0"
+    lastest = requests.get(lastest_url).json()
+    context = {"soon": soon, "release": release,
+               "top": top, "lastest": lastest}
+    return render(request, "home.html", context)
+
 
 def movies(request):
     return render(request, "movies.html")
 
+
 def login(request):
     return render(request, "login.html")
+
 
 def celebrities(request):
     return render(request, "celebrities.html")
 
-def moviedetails(request,pk):
+
+def moviedetails(request, pk):
     print(pk)
-    url='https://imdb-api.com/en/API/Title/k_95qlekw4/pk/Trailer,Ratings,Wikipedia'
-    details=requests.get(url).json()
-    context={"details":details}
-    
-    return render(request, "movie-details.html",context)  
+    url = 'https://imdb-api.com/en/API/Title/k_lwm5x736/'+pk+'/Trailer,Ratings,Wikipedia,'
+    details = requests.get(url).json()
+    context = {"details": details}
+
+    return render(request, "movie-details.html", context)
+
 
 def top_movies(request):
     return render(request, "top-movies.html")
 
+
 def blog(request):
     return render(request, "blog.html")
 
+
 def blog_details(request):
     return render(request, "blog-details.html")
+
 
 def register_user(request):
 
@@ -86,11 +94,11 @@ def register_user(request):
 
 
 def searchresult(request):
-    
+
     if request.method == "POST":
-        Query=request.POST.get("Query")
-        query_url="https://imdb-api.com/en/API/SearchMovie/k_08ug9l32/"+Query
-        query=requests.get(query_url).json()
+        Query = request.POST.get("Query")
+        query_url = "https://imdb-api.com/en/API/SearchMovie/k_08ug9l32/"+Query
+        query = requests.get(query_url).json()
         print(query)
-        context={"query":query}
-    return render(request,'result.html',context)    
+        context = {"query": query}
+    return render(request, 'result.html', context)
